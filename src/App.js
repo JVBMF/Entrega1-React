44579537f0606d59eb5d5768cd81982b318bd761
JVBMF <juann.decibe@gmail.com>
2022-02-22 14:49:06 -0300

    guardo antes de trabajar en la branch navBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
-import { NavBar } from './components/NavBar/NavBar';
+import './App.css'
+import { NavBar } from './components/NavBar/NavBar'
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        NavBar / >
-        <
-        /div>
+    return ( 
+        <div className="App"> 
+        <NavBar /> 
+        </div>
     );
 }
 
